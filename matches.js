1d98ed833f54c24a345c0cdf6cb0f716daf3d8c4
toniev <ivantoniev2@gmail.com>
2014-07-01 20:31:10 +0300

    using the matches info

--- matches.js
+++ matches.js
@@ -1,1 +1,23 @@
 'use strict';
+$.ajax({
+  url:'http://worldcup.sfg.io/matches/today',
+}).success(function(matches){
+  //console.log(matches);
+  var source   = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  var context = {};
+
+  matches.forEach(function(match){
+    context = {time_left: match.datetime, first_team_name: match.away_team.country,
+               second_team_name: match.home_team.country,
+               first_team_goals: match.away_team.goals,
+               second_team_goals: match.home_team.goals};
+
+    var pesho = match.home_team.country;
+    console.log(countrynames.getCode(pesho));
+
+    var html = template(context);
+
+    $('body').append(html);
+  });
+});
