168e080e56ebbb7044ad95728eb57f35339aa2c1
toniev <ivantoniev2@gmail.com>
2014-07-01 22:01:20 +0300

    basic matches view is ready. time left until start. time left until end. match over. flags foreach country. scores.

--- matches.js
+++ matches.js
@@ -2,22 +2,32 @@
 $.ajax({
   url:'http://worldcup.sfg.io/matches/today',
 }).success(function(matches){
-  //console.log(matches);
+  console.log(matches);
   var source   = $('#entry-template').html();
   var template = Handlebars.compile(source);
   var context = {};
+  var timer = '';
 
   matches.forEach(function(match){
-    context = {time_left: match.datetime, first_team_name: match.away_team.country,
-               second_team_name: match.home_team.country,
+    var time = Date.parse(match.datetime);
+    var timeDifference = parseInt(((Date.now() - parseInt(time, 10))/1000)/60);
+
+    if(timeDifference > 0) {
+      if(timeDifference > 90) {
+        timer = 'The match is over!';
+      } else {
+        timer = 'Time left: ' + timeDifference;
+      }
+    } else {
+      timer = 'Starts in: ' + (-timeDifference) + ' minutes';
+    }
+
+    context = {time_left: timer, first_team_name: 'flags/' + match.away_team.code + '.png',
+               second_team_name: 'flags/' + match.home_team.code + '.png',
                first_team_goals: match.away_team.goals,
                second_team_goals: match.home_team.goals};
 
-    var pesho = match.home_team.country;
-    console.log(countrynames.getCode(pesho));
-
     var html = template(context);
-
     $('body').append(html);
   });
 });
